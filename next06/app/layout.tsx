096c66790c15bd22b030ec914a108ff4951ce824
Alexis Fonte <alexis.e.fonte@gmail.com>
2023-07-26 13:11:03 -0500

    chapter 6 project setup

--- next06/app/layout.tsx
+++ next06/app/layout.tsx
@@ -12,7 +12,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className='dark:bg-slate-800'>{children}</body>
     </html>
   )
 }
