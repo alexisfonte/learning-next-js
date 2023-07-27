54e18f2d3c981922441813b8977e2d23beaa9626
Alexis Fonte <alexis.e.fonte@gmail.com>
2023-07-27 12:57:08 -0500

    added embedded images and youtube videos to posts from mdx

--- next12/lib/posts.ts
+++ next12/lib/posts.ts
@@ -2,6 +2,8 @@ import { compileMDX } from "next-mdx-remote/rsc";
 import rehypeAutolinkHeadings from "rehype-autolink-headings/lib";
 import rehypeHighlight from "rehype-highlight/lib";
 import rehypeSlug from "rehype-slug";
+import Video from "@/app/components/Video";
+import CustomImage from "@/app/components/CustomImage";
 
 type Filetree = {
   tree: [
@@ -37,6 +39,10 @@ export async function getPostByName(
     tags: string[];
   }>({
     source: rawMDX,
+    components: {
+      Video,
+      CustomImage
+    },
     options: {
       parseFrontmatter: true,
       mdxOptions: {
